0b6045ded8d98c6daae15de2a5df733cfffce883
johnkegz <johnkal24@gmail.com>
2020-04-05 21:46:52 +0300

    add objectives

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home/Home";
 import Aboutus from "./components/Aboutus/Aboutus"
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Page from "./components/Layout/page";
 import Activities from './components/Activities';
+import Objectives from './components/Objectives';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
           <Route exact path='/' component={Home}/>
           <Route path='/aboutus' component={Aboutus}/>
           <Route path='/activities' component={Activities}/>
+          <Route path='/objectives' component={Objectives}/>
         </Page>
         </Switch>
       </Router>
